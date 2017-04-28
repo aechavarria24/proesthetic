8d8be70f8f2759d1d48685322bf6105cd5dc0f58
aechavarria24 <aechavarria24@misena.edu.co>
2017-04-28 04:20:43 -0500

    Agregar medidas detalle pedido

--- resources/views/pedido/detalle.blade.php
+++ resources/views/pedido/detalle.blade.php
@@ -3,45 +3,79 @@
 @endsection
 @section('contenedor')
 <div class="box">
-  <div class="box-header">
-    <h2>Pedido</h2>
-  </div>
-  <div class="box-body">
-    <div class="row">
-      <div class="col-sm-4 offset-sm-4">
-        {{csrf_field()}}
-        <div class="form-group">
-            @foreach ($pedido as $valor)
-            @endforeach
-          <label>Número pedido </label>
-          <input class="form-control" required="" data-parsley-id="136" type="text" name="id" readonly="" value="{{$valor->id}}">
-        </div>
-        <div class="form-group">
-          <label>Clinica</label>
-          <input class="form-control" required="" data-parsley-id="136" type="text" name="idusuario" readonly="" value="{{$valor->nombre}}">
-        </div>
-        <div class="form-group">
-          <label>Fecha entrega</label>
-          <input class="form-control" required="" data-parsley-id="136" type="text" name="pedidoid" readonly="" value="{{$valor->fechaEntrega}}">
-        </div>
-        <div class="form-group">
-          <label>Paciente</label>
-          <input class="form-control" required="" data-parsley-id="136" type="text" name="pedidoid" readonly="" value="{{$valor->pacienteNombre}}">
-        </div>
-        <div class="form-group">
-          <label>Cedula paciente</label>
-          <input class="form-control" required="" data-parsley-id="136" type="text" name="pedidoid" readonly="" value="{{$valor->cedula}}">
-        </div>
-        <div class="form-group">
-          <label>Estado Pedido</label>
-          <input class="form-control" required="" data-parsley-id="136" type="text" name="pedidoid" readonly="" value="{{$valor->estadoPedido}}">
-        </div>
-        <div class=" p-a text-center">
-          <a class="btn btn-info" href="/pedido/show">Regresar</a>
-        </div>
-      </div>
+    <div class="box-header">
+        <h2>Pedido</h2>
     </div>
-  </div>
+    <div class="box-body">
+        <div class="row">
+            <div class="col-sm-4 offset-sm-1">
+                <div class="form-group">
+                    @foreach ($pedido as $valor)
+                    @endforeach
+                    <label>Número pedido </label>
+                    <input class="form-control" required="" data-parsley-id="136" type="text" name="id" readonly="" value="{{$valor->id}}">
+                </div>
+                <div class="form-group">
+                    <label>Clinica</label>
+                    <input class="form-control" required="" data-parsley-id="136" type="text" name="idusuario" readonly="" value="{{$valor->nombre}}">
+                </div>
+                <div class="form-group">
+                    <label>Fecha entrega</label>
+                    <input class="form-control" required="" data-parsley-id="136" type="text" name="pedidoid" readonly="" value="{{$valor->fechaEntrega}}">
+                </div>
+                <div class="form-group">
+                    <label>Paciente</label>
+                    <input class="form-control" required="" data-parsley-id="136" type="text" name="pedidoid" readonly="" value="{{$valor->pacienteNombre}}">
+                </div>
+                <div class="form-group">
+                    <label>Cedula paciente</label>
+                    <input class="form-control" required="" data-parsley-id="136" type="text" name="pedidoid" readonly="" value="{{$valor->cedula}}">
+                </div>
+                <div class="form-group">
+                    <label>Estado Pedido</label>
+                    <input class="form-control" required="" data-parsley-id="136" type="text" name="pedidoid" readonly="" value="{{$valor->estadoPedido}}">
+                </div>
+                <div class=" p-a text-center">
+                    <a class="btn btn-info" href="/pedido/show">Regresar</a>
+                </div>
+            </div>
+            <div class="col-sm-6">
+
+                <div class="box">
+                    <div class="box-header">
+                        Medidas de la pieza
+                    </div>
+                    <div class="box-body">
+
+
+                        @foreach ($servicios  as $key => $servicio)
+                        <label for="table">Pieza: <?php echo $servicio["servicio"] ?></label>
+                        <table class="table" id = "table">
+                            <thead>
+                                <tr>
+                                    <th>Cantidad</th>
+                                    <th>Dimesión</th>
+                                    <th>Unidad de medida</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($medidas_pieza  as $key => $pieza)
+                                @if($servicio["servicio"] == $pieza["servicio"])
+                                <tr>
+                                    <td><?php echo $pieza["cantidad"] ?></td>
+                                    <td><?php echo $pieza["dimension"] ?></td>
+                                    <td><?php echo $pieza["unidadMedidad"] ?></td>
+                                </tr>
+                                @endif
+                                @endforeach
+                            </tbody>
+                        </table>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 
